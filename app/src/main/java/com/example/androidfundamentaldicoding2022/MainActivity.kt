56dfc589d1d48fa6723c7223cd0bec41757b21b3
NawfalHadi <nawfalhdi@gmail.com>
2022-10-01 16:20:06 +0800

    declare move for result activity in AndroidManifest.xml

--- app/src/main/java/com/example/androidfundamentaldicoding2022/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentaldicoding2022/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.androidfundamentaldicoding2022
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.example.androidfundamentaldicoding2022.ui.MoveForResultActivity
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
