0b74438b7a7bc9d0d73a6a4156121b5190cd8593
Nawfal Hadi <nawfalhdi@gmail.com>
2022-10-18 22:24:42 +0800

    scorllview

---
+++ app/src/main/java/com/example/androidfundamentaldicoding2022/ui/dialog/ScrollViewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidfundamentaldicoding2022.ui.dialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidfundamentaldicoding2022.R
+
+class ScrollViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scroll_view)
+    }
+}
